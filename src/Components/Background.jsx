d1fb0f79e16cf5eddd1e3f063dff0e921500bdaa
ArpitKrRespect <arpitkr2020@gmail.com>
2024-12-26 01:55:20 +0530

    Initial Commit

---
+++ src/Components/Background.jsx
@@ -0,0 +1,14 @@
+const Background = () => {
+  return (
+    <>
+    <div className="absolute top-[5%] w-full py-10 flex justify-center text-zinc-600 text-xl font-semibold ">
+          Document
+    </div>
+    <h1 className=" absolute font-semibold top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] text-zinc-500 leading-none tracking-tighter "> 
+        Docs
+    </h1> 
+    </>
+  )
+}
+
+export default Background
